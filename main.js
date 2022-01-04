e313c8f4c1b8892aa6f466d8990610e6dab5644b
Douglas Oliveira <83471539+dougsoa@users.noreply.github.com>
2022-01-04 20:02:39 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const pesquisarCep = () => {
+  const cep = document.getElementById('cep').value
+  const url = `http://viacep.com.br/ws/${cep}/json/`
+  fetch(url).then(console.log)
+}
+
+document.getElementById('cep').addEventListener('focusout', pesquisarCep)
